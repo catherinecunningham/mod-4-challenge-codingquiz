36a00a6455060b7507e46ef4a36496821a2495e3
catherinecunningham <cfunk512@gmail.com>
2022-07-27 11:01:47 -0700

    changes with tutor help to all files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
-// start button begins timer - add event listener on button click
+//global variables
+var intro = document.getElementById("intro");
+var questionBox = document.getElementById("question-box");
 var timerEl = document.getElementById('countdown');
-timerEl.addEventListener('click', function() {
-// basic countdown setup
+var startButton = document.getElementById("start-button");
+var questionIndex = 0;
+var question = document.getElementById("question");
+var option1 = document.getElementById("option-1");
+var option2 = document.getElementById("option-2");
+var option3 = document.getElementById("option-3");
+var option4 = document.getElementById("option-4");
+
+// start button begins timer - add event listener on button click
+
 function countdown() {
     var timeLeft = 45;
     var timeInterval = setInterval(function() {
         if (timeLeft > 1) {
-            timerEl.textcontent = timeLeft + ' seconds remaining';
-            timeleft--;
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
         }
         else if (timeLeft === 1) {
             timerEl.textContent = timeLeft + ' second remaining';
@@ -15,18 +25,19 @@ function countdown() {
         else {
             timerEl.textContent = '';
             clearInterval(timeInterval);
-            displayMessage();
+            //displayMessage();
         }
     }, 1000);
-}
-});
+};
 
-countdown();
+// countdown();
 
 // timer in top right corner counts down
 
 //question content
-// setup for the questions, repeat with actual question content
+
+//object obj.key = val
+// answer key is another object, answers could also be an array
 var quizQuestions = [
     {
         question: "kddkjfsdkjf",
@@ -36,51 +47,58 @@ var quizQuestions = [
             C: 'c',
         },
         correctAnswer: 'b'
+    },
+    {
+        question: "sldkjfldskjf",
+        answers: {
+            A: 'a',
+            B: 'b',
+            C: 'c',
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "sldkjjf",
+        answers: {
+            A: 'a',
+            B: 'b',
+            C: 'c',
+        },
+        correctAnswer: 'a'
     } //add comma here and insert additional questions
 ];
+
+for(var i=0; i < quizQuestions.length; i++) {
+    console.log(i);
+    console.log(quizQuestions[i]);
+    console.log(quizQuestions[i].question);
+}
+
+function displayQuestion() {
+    question.textContent = quizQuestions[questionIndex].question;
+    option1.textContent = quizQuestions[questionIndex].answers.A;
+    option2.textContent = quizQuestions[questionIndex].answers.B;
+    option3.textContent = quizQuestions[questionIndex].answers.C;
+    
+}
+
+// when we click the start button, countdown starts and the questions display
+function startGame() {
+    intro.style.display = "none";
+    questionBox.style.display = "block";
+    countdown();
+    displayQuestion();
+}
 
 // add "Wrong" and "Correct" indicator
 // if question correct, move on to next question AND add to score
 // if question wrong, REMOVE time from timer AND move on to next question
-
-// place to store the output and answer choices
-function showQuestions(questions,quizContainer){
-var output = [];
-var answers;
-
-//for loop to show the questions and available answers
-for(var i=0; i<quizQuestions.length; i++){
-    answers = [];
-}
-
-// reset the list of answers
-answers = [];
-
-// for each available answer to this question...
-for(letter in questions[i].answers){
-    //add html radio button
-    answers.push(
-        '<label>'
-            + '<input type="radio" name="question' +i+'" value ="'+letter+'">'
-            + letter + '; '
-            + questions[i].answers[letter]
-        + '<label>'
-    );
-}
-
-// combine output list into one string of html
-quizContainer.innerHTML = output.join('');
-}
-
-// add this question and its answers to the output
-output.push(
-    '<div class="question">' + questions[i].question + '<div>'
-);
-
-showQuestions(questions,quizContainer);
 
 // timer runs out
 // end of quiz
 // input name 
 // quiz records the score or you input the score yourself??
 // score and name recorded 
+
+//event listener
+startButton.addEventListener('click', startGame)
