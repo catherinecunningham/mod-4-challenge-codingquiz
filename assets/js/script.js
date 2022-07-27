e9f7132abd942aecf73db2e14e7d81bab288f6d9
catherinecunningham <cfunk512@gmail.com>
2022-07-26 19:20:14 -0700

    add comments in all three files to prep for code input

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// start button begins timer
+
+// timer in top right corner counts down
+
+// question correct answer leads to another question
+// question INCORRECT answer removes time from the timer in top right corner
+    // then moves on to another question
+
+// timer runs out
+// end of quiz
+// input name 
+// quiz records the score or you input the score yourself??
+// score and name recorded 
