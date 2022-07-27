7cbf3a998f2539377d2d98f91f44903796950dbd
catherinecunningham <cfunk512@gmail.com>
2022-07-26 20:13:25 -0700

    input example code in js, update comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,59 @@
 
 // timer in top right corner counts down
 
-// question correct answer leads to another question
-// question INCORRECT answer removes time from the timer in top right corner
-    // then moves on to another question
+//question content
+// setup for the questions, repeat with actual question content
+var quizQuestions = [
+    {
+        question: "kddkjfsdkjf",
+        answers: {
+            A: 'a', //place comma after each answer
+            B: 'b',
+            C: 'c',
+        },
+        correctAnswer: 'b'
+    } //add comma here and insert additional questions
+];
+
+// add "Wrong" and "Correct" indicator
+// if question correct, move on to next question AND add to score
+// if question wrong, REMOVE time from timer AND move on to next question
+
+// place to store the output and answer choices
+function showQuestions(questions,quizContainer){
+var output = [];
+var answers;
+
+//for loop to show the questions and available answers
+for(var i=0; i<quizQuestions.length; i++){
+    answers = [];
+}
+
+// reset the list of answers
+answers = [];
+
+// for each available answer to this question...
+for(letter in questions[i].answers){
+    //add html radio button
+    answers.push(
+        '<label>'
+            + '<input type="radio" name="question' +i+'" value ="'+letter+'">'
+            + letter + '; '
+            + questions[i].answers[letter]
+        + '<label>'
+    );
+}
+
+// combine output list into one string of html
+quizContainer.innerHTML = output.join('');
+}
+
+// add this question and its answers to the output
+output.push(
+    '<div class="question">' + questions[i].question + '<div>'
+);
+
+showQuestions(questions,quizContainer);
 
 // timer runs out
 // end of quiz
